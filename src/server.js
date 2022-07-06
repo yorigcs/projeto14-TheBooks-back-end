e23c0fa0d368f9aa0a9c01ad9bac87722c94308a
Yorí <yorigcsdev@gmail.com>
2022-07-06 00:55:54 -0300

    Feature: add public route -> /books

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import publicRoutes from './routes/publicRoute.js'
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -5,6 +6,6 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const server = express();
 
-server.use([cors(), express.json()]);
+server.use([cors(), express.json(), publicRoutes]);
 
 server.listen(PORT, () => console.log(`listening on port ${PORT}`));
