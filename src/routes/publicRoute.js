10a1beaf99f474968a8821e8f0473e2e142fb480
Yorí <yorigcsdev@gmail.com>
2022-07-06 02:23:03 -0300

    Feature: add route /Books controller for lastBooks

--- src/routes/publicRoute.js
+++ src/routes/publicRoute.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
-
+import getBooksController from "../controller/getBooksController.js";
 const routes = Router();
 
-routes.get("/books")
+routes.get("/books",getBooksController)
 
 export default routes;
