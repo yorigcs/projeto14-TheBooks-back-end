b6a1c036bb0778672198c284451f899adef801b6
Yorí <yorigcsdev@gmail.com>
2022-07-07 18:49:05 -0300

    Feature: add signUpValidate

--- src/routes/publicRoute.js
+++ src/routes/publicRoute.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 import getBooksController from "../controller/getBooksController.js";
 import getMostRatedBooksController from "../controller/getMostRatedBooksController.js";
 import getBestSellersController from "../controller/getBestSellersController.js";
-
+import validateSignUp from "../middlewares/publicMiddlewares/validateSignUp.js";
 const routes = Router();
 
-routes.get("/books",getBooksController)
+routes.get("/books", getBooksController)
 routes.get("/mostRatedBooks", getMostRatedBooksController)
 routes.get("/bestSellersBooks", getBestSellersController)
+routes.post("/sign-up", validateSignUp)
 
 export default routes;
