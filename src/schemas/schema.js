bf6fa2096ca1891827f0215dce3570f9695d3bc1
Yorí <yorigcsdev@gmail.com>
2022-07-07 19:23:48 -0300

    Fix: fix signUpSchma email

--- src/schemas/schema.js
+++ src/schemas/schema.js
@@ -4,7 +4,7 @@ const signUpSchema = joi.object({
     name: joi.string().required(),
     password: joi.string().required(),
     cpf: joi.string().required(),
-    email: joi.email().required(),
+    email: joi.string().email().required(),
 });
 
 export {signUpSchema}
