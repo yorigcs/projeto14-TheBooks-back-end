61a8b544cc4d8531393869420eea245efbe2d52c
andreseichi <andre_seichi@hotmail.com>
2022-07-08 13:48:20 -0300

    feat: book page api && route

--- src/routes/publicRoute.js
+++ src/routes/publicRoute.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import getBooksController from "../controller/getBooksController.js";
 import getMostRatedBooksController from "../controller/getMostRatedBooksController.js";
 import getBestSellersController from "../controller/getBestSellersController.js";
+import getBookController from "../controller/getBookController.js";
 
 const routes = Router();
 
 routes.get("/books",getBooksController)
+routes.get("/book/:id", getBookController)
 routes.get("/mostRatedBooks", getMostRatedBooksController)
 routes.get("/bestSellersBooks", getBestSellersController)
 
