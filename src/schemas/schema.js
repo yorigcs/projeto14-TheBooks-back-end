d851b63a7f4a936d929f6f0616609df83ea9f56c
Yorí <yorigcsdev@gmail.com>
2022-07-08 14:46:32 -0300

    Fix: change cpf's logic schema

--- src/schemas/schema.js
+++ src/schemas/schema.js
@@ -3,7 +3,7 @@ import joi from 'joi';
 const signUpSchema = joi.object({
     name: joi.string().required(),
     password: joi.string().required(),
-    cpf: joi.string().required(),
+    cpf: joi.string().required().length(11).pattern(/^[0-9]*$/ ),
     email: joi.string().email().required(),
 });
 
