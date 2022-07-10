d54906af7dc8497389c1c401569c9aebbcdee6dc
Yorí <yorigcsdev@gmail.com>
2022-07-10 17:15:29 -0300

    Fix: change name email-> user: email

--- src/middlewares/privateMiddlewares/validateJwtToken.js
+++ src/middlewares/privateMiddlewares/validateJwtToken.js
@@ -4,17 +4,16 @@ dotenv.config();
 const SECRET = process.env.SECRET_KEY || "!5S5G6$1AE@";
 
 const validateJwtToken = (req, res, next) => {
- const token = req.header("Authorization")?.replace("Bearer ","");
- const { email } = req.body;
- try {
-    const decodedToken = jwt.verify(token, SECRET)
-    if (email !== decodedToken.email) {
-        return res.status(401).send("Não autorizado!")
+    const token = req.header("Authorization")?.replace("Bearer ", "");
+    const { user } = req.body;
+    try {
+        const decodedToken = jwt.verify(token, SECRET)
+        if (user !== decodedToken.email) {
+            return res.status(401).send("Não autorizado!")
+        }
+        next();
+    } catch (err) {
+        return res.status(401).send("Token inválido!")
     }
-    next();
-} catch (err) {
-    console.log(req.headers)
-    return res.status(401).send("Token inválido!")
-}    
 }
 export default validateJwtToken;
