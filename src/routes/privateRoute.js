133355e3eacb3d94859663b4940d8da7c84ff1e0
Yorí <yorigcsdev@gmail.com>
2022-07-10 17:55:35 -0300

    Feature: create order

--- src/routes/privateRoute.js
+++ src/routes/privateRoute.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
+import createOrderController from "../controller/createOrderController.js";
 import updateProfileController from "../controller/updateProfileController.js";
+import validateCreateOrder from "../middlewares/privateMiddlewares/validateCreateOrder.js";
 import validateJwtToken from "../middlewares/privateMiddlewares/validateJwtToken.js";
 import validateUpdateProfille from "../middlewares/privateMiddlewares/validateUpdateProfille.js";
 
 const router = Router();
 router.put("/updateProfile",validateJwtToken, validateUpdateProfille, updateProfileController);
+router.post("/orders",validateJwtToken,validateCreateOrder,createOrderController )
 
 
 export default router;
