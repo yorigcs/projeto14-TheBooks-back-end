97dd5fc0fce239248de66fbfc42c45c340404cfa
Yorí <yorigcsdev@gmail.com>
2022-07-11 14:03:49 -0300

    Feature: add myOrders controller

--- src/controller/getMyOrdersController.js
+++ src/controller/getMyOrdersController.js
@@ -1,4 +1,14 @@
+import mongoDB from "../database/mongoDB";
 const getMyOrdersController = async (req, res) => {
+    const { user } = req.body;
+    try {
+        const myOrders = await mongoDB.collections("orders").find({ user }).toArray();
+        res.send(myOrders)
+
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send("Houve um erro ao consultar!")
+    }
 
 }
 export default getMyOrdersController;
